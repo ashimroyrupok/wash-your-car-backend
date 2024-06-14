cd17534768c4260c1de0bd2dfa18dd51afa47fef
ashimroyrupok <ashimroyrupok@gmail.com>
2024-06-14 15:24:31 +0600

    create catchAsync function

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.use("api/v1", router);
+app.use("api/", router);
 
 app.get('/', (req, res) => {
     res.send("server is running")
